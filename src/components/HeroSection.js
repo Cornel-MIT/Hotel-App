26e6797b925720af5065ca4f7f0ec9d54358a272
Cornel <www.ccornel@gmail.com>
2024-10-07 10:50:16 +0200

    Worked on the UI

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDocs, collection } from 'firebase/firestore'; 
+import { db } from '../firebase';
 import './HeroSection.css';
-import image1 from '../media/villa-simonne-main-enterance.jpg';
-import image2 from '../media/rock-pool-and-our-executive.jpg';
-import image3 from '../media/heated-pool-outside-bedrooms.jpg';
 
 const HeroSection = () => {
-  const images = [image1, image2, image3];
-
+  const [images, setImages] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const roomsCollection = collection(db, 'rooms');
+      const roomSnapshot = await getDocs(roomsCollection);
+      const roomList = roomSnapshot.docs.map((doc) => doc.data());
+
+      const roomImages = roomList.map((room) => room.imageUrl); 
+      setImages(roomImages);
+    };
+
+    fetchRooms();
+  }, []); 
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 10000); 
+
+    return () => clearInterval(interval); 
+  }, [images]);
 
   const nextImage = () => {
     setCurrentIndex((prevIndex) =>
@@ -23,9 +44,11 @@ const HeroSection = () => {
 
   return (
     <div className="hero-section">
-      <img src={images[currentIndex]} alt="Hero Image" className="hero-image" />
+      {images.length > 0 && (
+        <img src={images[currentIndex]} alt="Room Image" className="hero-image" />
+      )}
       <div className="hero-overlay">
-        <h1  className='slogan'>Welcome to Eldiablo</h1>
+        <h1 className='slogan'>Welcome to Eldiablo</h1>
         <p>Welcome to Eldiablo where we take care of all your desires</p>
       </div>
       <button className="prev" onClick={prevImage}>
