6343820af6e786a49b7eec127f750c27694202ec
Cornel <www.ccornel@gmail.com>
2024-10-24 14:43:26 +0200

    Updated Rooms Page

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { db } from '../firebase';
+import Footer from './Footer'
 import './Rooms.css'; 
 
 const StarRating = ({ rating }) => {
@@ -49,29 +50,33 @@ const Rooms = () => {
   }
 
   return (
-    <section className="rooms">
-      <h2>All Available Rooms</h2>
-      <div className="rooms-container">
-        {rooms.map((room) => (
-          <div 
-            key={room.id} 
-            className="room-card" 
-            onClick={() => navigate(`/user/booking`, { state: { roomData: room } })}
-          >
-            <img src={room.imageUrl} alt={room.roomName} />
-            <h3>{room.roomName}</h3>
-            <p>{room.description}</p>
-            <p>Price: <strong>R</strong> {room.price} per night</p>
-            <p>Available for {room.availableDays} days</p>
-            <p>Adults: {room.adults}, Children: {room.children}</p>
-            <div className="rating-container">
-              <StarRating rating={Math.round(room.averageRating || 0)} />
-              <span>({room.totalRatings || 0} ratings)</span>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+    <div>
+        <section className="rooms">
+         <h2>All Available Rooms</h2>
+         <div className="rooms-container">
+           {rooms.map((room) => (
+             <div 
+               key={room.id} 
+               className="room-card" 
+               onClick={() => navigate(`/user/booking`, { state: { roomData: room } })}
+             >
+               <img src={room.imageUrl} alt={room.roomName} />
+               <h3>{room.roomName}</h3>
+               <p>{room.description}</p>
+               <p>Price: <strong>R</strong> {room.price} per night</p>
+               <p>Available for {room.availableDays} days</p>
+               <p>Adults: {room.adults}, Children: {room.children}</p>
+               <div className="rating-container">
+                 <StarRating rating={Math.round(room.averageRating || 0)} />
+                 <span>({room.totalRatings || 0} ratings)</span>
+               </div>
+             </div>
+           ))}
+         </div>
+         
+      </section>
+        <Footer/>
+    </div>
   );
 };
 
